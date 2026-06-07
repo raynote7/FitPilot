4b5bdc1327fe6917bf52f456ea0619d8d2513a4f
raynote7 <166900308+raynote7@users.noreply.github.com>
2026-06-07 23:38:42 +0900

    Support Firebase Hosting base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const base = process.env.VITE_BASE_PATH || '/';
+
 export default defineConfig({
-  base: '/FitPilot/',
+  base,
   plugins: [react()],
 });
